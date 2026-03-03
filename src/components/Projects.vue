08515904f65498a82aa1b74ad124a4c9c00deb0d
joeyaquino25 <joeyaquino1925@gmail.com>
2026-03-03 21:19:18 +0800

    Add files via upload

---
+++ src/components/Projects.vue
@@ -0,0 +1,42 @@
+<template>
+	<section class="pb-5" id="projects">
+	    <h1 class="mt-5 mb-5 pb-4 text-center">My Projects</h1>
+	    <div 
+	    	v-for="(group, index) in chunkedProjects"
+	    	:key="index"
+	    	class="card-deck my-5 justify-content-center"
+	    >
+	    	<ProjectCard 
+	    		v-for="project in group"
+	    		:key="project.id"
+	    		:project="project"
+	    	/>
+	    	
+	    </div>
+
+	    <!-- BREAK: Be back at 7:32 PM -->
+	 
+
+	    <!-- End of 3rd Projects Placeholder-->
+	</section>
+</template>
+
+<script setup>
+	
+	import {computed} from "vue";
+	import ProjectCard from "./ProjectCard.vue";
+	import projects from "../data/projects.json";
+
+	const chunkSize = 3;
+
+	const chunkedProjects = computed(() => {
+
+		const chunks = [];
+
+		for(let i = 0; i < projects.length; i+=chunkSize){
+			chunks.push(projects.slice(i, i+chunkSize));
+		}
+
+		return chunks;
+	})
+</script>
